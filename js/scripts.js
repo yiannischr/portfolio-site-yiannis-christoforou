f5fedb3c543be3236e7495fb80e075ef523a1cc8
yiannischr <yiannis.chr00@gmail.com>
2024-07-29 13:56:35 +0300

    exercise2.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hi! Welcome to My Portfolio Site")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+      } else {
+        x.className = 'navtoggle';
+      }
+}
