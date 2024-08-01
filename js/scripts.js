b91dcbf51a1329a18cb663f91d613e2976398610
yiannischr <yiannis.chr00@gmail.com>
2024-08-01 14:43:36 +0300

    exercise2.3 feedback

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-        x.className += ' responsive';
-      } else {
-        x.className = 'navtoggle';
-      }
+var x = document.getElementById('myNavtoggle');
+if (x.className === 'navtoggle' && $(window).width() < 640) {
+x.className += ' responsive';
+} else {
+x.className = 'navtoggle';
+}
 }
